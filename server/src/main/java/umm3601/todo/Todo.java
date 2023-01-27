a28cabcb91eb674ec5a67cb000293f4c5e029a31
Alex <trinc005@morris.umn.edu>
2023-01-27 15:23:27 -0600

    Completed Todo.java Added code into todo.java

---
+++ server/src/main/java/umm3601/todo/Todo.java
@@ -0,0 +1,14 @@
+package umm3601.todo;
+
+@SuppressWarnings({"VisibilityModifier"})
+
+public class Todo {
+  @SuppressWarnings({"MemberName"})
+  public String _id;
+  public String owner;
+  public String category;
+  public Boolean status;
+  public String body;
+
+
+}
