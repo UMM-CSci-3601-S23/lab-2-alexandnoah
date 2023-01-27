5917d2bb327596b4fd0e41cdbc0d031b8bd91f44
Alex <trinc005@morris.umn.edu>
2023-01-27 15:26:04 -0600

    Completed TodoDatabase.java Added code to todo.java from our old file.

--- server/src/main/java/umm3601/todo/TodoDatabase.java
+++ server/src/main/java/umm3601/todo/TodoDatabase.java
@@ -1,5 +1,80 @@
 package umm3601.todo;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class TodoDatabase {
+
+  private Todo[] allTodos;
+
+  public TodoDatabase(String todoDataFile) throws IOException {
+    InputStreamReader reader = new InputStreamReader(getClass().getResourceAsStream(todoDataFile));
+    ObjectMapper objectMapper = new ObjectMapper();
+    allTodos = objectMapper.readValue(reader, Todo[].class);
+  }
+
+  public int size() {
+    return allTodos.length;
+  }
+
+  public Todo getTodo(String id) {
+    return Arrays.stream(allTodos).filter(x -> x._id.equals(id)).findFirst().orElse(null);
+  }
+
+  public Todo[] listTodos(Map<String, List<String>> queryParams) {
+    Todo[] filteredTodos = allTodos;
+
+    if (queryParams.containsKey("category")) {
+      String targetCategory = queryParams.get("category").get(0);
+      filteredTodos = filterTodosByCategory(filteredTodos, targetCategory);
+    }
+    if (queryParams.containsKey("owner")) {
+      String targetOwner = queryParams.get("owner").get(0);
+      filteredTodos = filterTodosByOwner(filteredTodos, targetOwner);
+    }
+    if (queryParams.containsKey("body")) {
+      String targetBody = queryParams.get("body").get(0);
+      filteredTodos = filterTodosByBody(filteredTodos, targetBody);
+    }
+    if (queryParams.containsKey("status")) {
+      boolean targetBoolean = false;
+      String targetBody = queryParams.get("status").get(0);
+      if (targetBody.equals("complete")) {
+        targetBoolean = true;
+      }
+      filteredTodos = filterTodosByStatus(filteredTodos, targetBoolean);
+    }
+
+      return filteredTodos;
+    }
+
+  public Todo getTodos(String id) {
+    return Arrays.stream(allTodos).filter(x -> x._id.equals(id)).findFirst().orElse(null);
+  }
+
+  public Todo[] filterTodosByCategory(Todo[] todos, String targetCategory) {
+    return Arrays.stream(todos).filter(x -> x.category.equals(targetCategory)).toArray(Todo[]::new);
+  }
+
+  public Todo[] filterTodosBy_id(Todo[] todos, String target_id) {
+    return Arrays.stream(todos).filter(x -> x._id.equals(target_id)).toArray(Todo[]::new);
+  }
+
+  public Todo[] filterTodosByOwner(Todo[] todos, String targetOwner) {
+    return Arrays.stream(todos).filter(x -> x.owner.equals(targetOwner)).toArray(Todo[]::new);
+  }
+
+  public Todo[] filterTodosByStatus(Todo[] todos, boolean targetStatus) {
+    return Arrays.stream(todos).filter(x -> x.status.equals(targetStatus)).toArray(Todo[]::new);
+  }
+
+  public Todo[] filterTodosByBody(Todo[] todos, String targetBody) {
+    return Arrays.stream(todos).filter(x -> x.body.equals(targetBody)).toArray(Todo[]::new);
+  }
 
 }
