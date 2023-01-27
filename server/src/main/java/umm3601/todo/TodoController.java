d49c8031ba06cde03047804645ecaabf5a335e08
Alex <trinc005@morris.umn.edu>
2023-01-27 15:28:41 -0600

    Completes TodoController.java Added code to TodoDatabase.java from our old file.

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -1,5 +1,32 @@
 package umm3601.todo;
 
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import io.javalin.http.NotFoundResponse;
+
+
 public class TodoController {
+
+  private TodoDatabase database;
+
+  public TodoController(TodoDatabase database) {
+    this.database = database;
+  }
+
+  public void getTodo(Context ctx) {
+    String id = ctx.pathParam("id");
+    Todo todo = database.getTodo(id);
+    if (todo != null) {
+      ctx.json(todo);
+      ctx.status(HttpStatus.OK);
+    } else {
+      throw new NotFoundResponse("No Todo with id " + id + " was found.");
+    }
+  }
+
+  public void getTodos(Context ctx) {
+    Todo[] todos = database.listTodos(ctx.queryParamMap());
+    ctx.json(todos);
+  }
 
 }
